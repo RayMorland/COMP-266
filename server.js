136076f45dfd0007d3ca03e5ce1629d5aa0d4d28
Raymond Morland <30455116+RayMorland@users.noreply.github.com>
2023-02-08 12:12:40 -0800

    Work on node

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const port = 3000;
 
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+})
+
 // app.use('/unit-1/investo-website', require('./unit-1/unit1'));
 app.use('/unit-2/investo-website', require('./unit-2/unit2'));
 app.use('/unit-3/investo-website', require('./unit-3/unit3'));
@@ -15,13 +19,9 @@ app.use('/unit-4/investo-website', require('./unit-4/unit4'));
 
 // app.use('/api', unit4Routes);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-})
-
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + "/404.html");
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(__dirname + "/404.html");
+// })
   
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
