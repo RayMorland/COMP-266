92bdbae05f05455efbdfb31519dea6383100e84a
Raymond Morland <30455116+RayMorland@users.noreply.github.com>
2023-02-08 12:01:40 -0800

    Work on node

--- unit-4/unit4.js
+++ unit-4/unit4.js
@@ -2,14 +2,14 @@ var express = require("express");
 var path = require("path");
 var unit4 = express.Router();
 
-// unit4.use(
-//   "/assets/",
-//   express.static(path.join(__dirname, "/investo-website/assets"))
-// );
-
 unit4.use(
-    express.static(path.join(__dirname, "/investo-website"))
-  );
+  "/assets/",
+  express.static(path.join(__dirname, "/investo-website/assets"))
+);
+
+// unit4.use(
+//     express.static(path.join(__dirname, "/investo-website"))
+//   );
 
 unit4.get("/index.html", (req, res) => {
   res.redirect(__dirname + "/investo-website/index.html");
