ba4a4ff6204bb9622939e56ace63b292af92a59c
Raymond Morland <30455116+RayMorland@users.noreply.github.com>
2023-02-18 16:03:43 -0800

    Copy unit 4 into unit 5

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get('/index.html', (req, res) => {
 app.use('/unit-2/investo-website', require('./unit-2/unit2'));
 app.use('/unit-3/investo-website', require('./unit-3/unit3'));
 app.use('/unit-4/investo-website', require('./unit-4/unit4'));
-// app.use('/unit-5/investo-website', require('./unit-5/unit5'));
+app.use('/unit-5/investo-website', require('./unit-5/unit5'));
 // app.use('/unit-6/investo-website', require('./unit-6/unit6'));
 // app.use('/unit-7/investo-website', require('./unit-7/unit7'));
 
---
+++ unit-5/unit5.js
@@ -0,0 +1,67 @@
+var express = require("express");
+var path = require("path");
+var unit4 = express.Router();
+var stockSymbols = require("../url-lists/stock-symbols");
+var articleList = require("../url-lists/article-list");
+const { nextTick } = require("process");
+
+unit4.use(
+  "/assets/",
+  express.static(path.join(__dirname, "/investo-website/assets"))
+);
+
+unit4.use(express.static(path.join(__dirname, "/investo-website")));
+
+unit4.get("/index.html", (req, res) => {
+  res.redirect(__dirname + "/investo-website/index.html");
+});
+
+unit4.get("/", (req, res) => {
+  res.sendFile(__dirname + "/investo-website/index.html");
+});
+
+unit4.get("/learn.html", (req, res) => {
+  res.sendFile(__dirname + "/investo-website/learn.html");
+});
+
+unit4.get("/learn/article", (req, res) => {
+  res.redirect("/unit-4/investo-website/learn.html");
+});
+
+unit4.get("/learn/article/*", (req, res, next) => {
+  let slug = req.url.split("/")[3];
+
+  if (articleList.indexOf(slug) >= 0) {
+    res.sendFile(__dirname + `/investo-website/articles/${slug}.html`);
+  } else {
+    next();
+  }
+});
+
+unit4.get("/invest.html", (req, res) => {
+  res.sendFile(__dirname + "/investo-website/invest.html");
+});
+
+unit4.get("/invest/portfolio.html", (req, res) => {
+  res.sendFile(__dirname + "/investo-website/invest/portfolio.html");
+});
+
+unit4.get("/invest/stock", (req, res) => {
+  res.redirect("/unit-4/investo-website/invest.html");
+});
+
+unit4.get("/invest/stock/*", (req, res, next) => {
+  let symbol = req.url.split("/")[3];
+
+  if (stockSymbols.indexOf(symbol) >= 0) {
+    res.sendFile(__dirname + `/investo-website/stocks/${symbol}.html`);
+  } else {
+    next();
+  }
+});
+
+unit4.get("/about.html", (req, res) => {
+  res.sendFile(__dirname + "/investo-website/about.html");
+});
+
+module.exports = unit4;
