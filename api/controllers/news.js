81a8a565e2c419135a740801224c386134714ccd
Raymond Morland <30455116+RayMorland@users.noreply.github.com>
2023-02-25 10:27:23 -0800

    Adding Stock News API

--- api/controllers/news.js
+++ api/controllers/news.js
@@ -1,13 +1,12 @@
 const axios = require("axios");
 
 module.exports.get = async (req, res) => {
-  //   await axios
-  //     .get(
-  //       `https://stocknewsapi.com/api/v1?tickers=${req.query.symbol}&items=3&page=1&token=${process.env.STOCK_NEWS_API_KEY}`
-  //     )
-  //     .then((data) => {
-  //       let articles = { articles: data.data.data.slice(0, 3) };
-  //       res.send(articles);
-  //     });
-  res.send({ message: "hello" });
+    await axios
+      .get(
+        `https://stocknewsapi.com/api/v1?tickers=${req.query.symbol}&items=3&page=1&token=${process.env.STOCK_NEWS_API_KEY}`
+      )
+      .then((data) => {
+        let articles = { articles: data.data.data.slice(0, 3) };
+        res.send(articles);
+      });
 };
