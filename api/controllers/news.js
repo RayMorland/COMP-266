99798dd0b3c4be1950fdec95073048be2fd4d184
Raymond Morland <30455116+RayMorland@users.noreply.github.com>
2023-02-25 09:42:07 -0800

    Adding Stock News API

--- api/controllers/news.js
+++ api/controllers/news.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
-module.exports.get = (req, res) => {
-  axios
+module.exports.get = async (req, res) => {
+  await axios
     .get(
       `https://stocknewsapi.com/api/v1?tickers=${req.query.symbol}&items=3&page=1&token=${process.env.STOCK_NEWS_API_KEY}`
     )
